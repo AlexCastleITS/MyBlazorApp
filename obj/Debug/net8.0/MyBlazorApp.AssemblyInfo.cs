7cb7453cbf514ecae9f1877011e35bfa7de06237
AlexCastleITS <acastellano@studenti.apuliadigitalmaker.it>
2025-09-11 11:59:10 +0200

    ciao

--- obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9439fa185458cbe685f0234e466bf8504fb40b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a182fe18c6b66109d914b1f9bf07cff1d9de1438")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
